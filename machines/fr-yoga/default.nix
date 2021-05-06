51e03fe3b4f5c449b12ca84fc831539b77986712
Frederik Rietdijk <fridh@fridh.nl>
2021-05-06 12:01:38 +0200

    yoga: don't use networking

--- machines/fr-yoga/default.nix
+++ machines/fr-yoga/default.nix
@@ -45,9 +45,9 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp3s0f4u1u1.useDHCP = true;
-  networking.interfaces.wlp1s0.useDHCP = true;
+  #networking.useDHCP = false;
+  #networking.interfaces.enp3s0f4u1u1.useDHCP = true;
+  #networking.interfaces.wlp1s0.useDHCP = true;
 
   hardware.pulseaudio = {
     enable = true;
