7da3273d9150826a720646ced9db92d01cbe7dfa
Frederik Rietdijk <fridh@fridh.nl>
2021-05-20 14:02:47 +0200

    remove old packages

--- users/freddy.nix
+++ users/freddy.nix
@@ -1,4 +1,3 @@
-with <nixpkgs> {};
 {
 
   allowUnfree = true;
@@ -10,33 +9,5 @@ with <nixpkgs> {};
   ];
 
   packageOverrides = pkgs: with pkgs; {
-
-    mypkgs = pkgs.callPackage ../packages { };
-#     inherit (mypkgs) pythonEnv;
-
-    ## Example environments. Using nix-shell instead.
-    #workEnv = pkgs.myEnvFun {
-    #    name = "work";
-    #    buildInputs = with python35Packages; [
-    #      python35
-    #      ipython
-    #      numpy
-    #      scipy
-    #      matplotlib
-    #      pandas
-    #      sympy
-    #      pytest
-    #      numexpr
-    #      networkx
-    #      h5py
-    #      toolz
-    #      cytoolz
-    #      cython
-    #      odo
-    #    ];
-    #};
-    #blogEnv = python35.withPackages (ps: [ps.pelican]);
-    #mailEnv = python27.withPackages (ps: with ps; [pyyaml offlineimap ]);
   };
-
 }
