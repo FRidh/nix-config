9727deb6517434ff091b452111acd17e6fe17787
Frederik Rietdijk <fridh@fridh.nl>
2022-06-06 10:54:23 +0200

    fr-desktop: stay at nixos-21.11
    Black screen at unstable and 22.05 with fr-desktop (nvidia + sddm).

--- flake.nix
+++ flake.nix
@@ -3,33 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-22.05";
+    nixos-2111.url = "github:nixos/nixpkgs?ref=nixos-21.11";
     #nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-21.05";
     #nixpkgs.url = "github:WilliButz/nixpkgs?ref=codimd/fix-sqlite/node12";
     #nixpkgs.url = "git+https://github.com/WilliButz/nixpkgs?ref=codimd/fix-sqlite/node12";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils } @ inputs: rec {
+  outputs = { self, nixpkgs, nixos-2111, utils } @ inputs: rec {
 
-    nixosConfigurations."fr-desktop" = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."fr-desktop" = let
+      nixpkgs = nixos-2111;
+    in nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         (import ./machines/fr-desktop/default.nix)
         #(builtins.toPath "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
       ];
-      specialArgs = { inherit inputs; };
+#      specialArgs = { inherit inputs; };
     };
 
     nixosConfigurations."fr-laptop" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ (import ./machines/fr-laptop/default.nix) ];
-      specialArgs = { inherit inputs; };
+#      specialArgs = { inherit inputs; };
     };
 
     nixosConfigurations."fr-yoga" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ (import ./machines/fr-yoga/default.nix) ];
-      specialArgs = { inherit inputs; };
+#      specialArgs = { inherit inputs; };
     };
 
     nixosConfigurations."server2" = let
@@ -41,7 +44,7 @@
         (import ./machines/server2/default.nix)
         (builtins.toPath "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix")
       ];
-      specialArgs = { inherit inputs; };
+#      specialArgs = { inherit inputs; };
     };
 
     #packages.x86_64-linux.defaultPackage = nixosConfigurations."fr-desktop";
